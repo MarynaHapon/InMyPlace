90ab94b3f443da0c862ae3c6b631dabb6011adba
Maryna Hapon <marynahapon@gmail.com>
2018-06-04 12:27:57 +0300

    mongoose / close connection

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,9 +1,31 @@
 var mongoose = require('mongoose');
 var readLine = require('readline');
+var gracefulShutdown;
 var dbURI = 'mongodb://localhost/InMyPlace';
 
 /* Mongoose connection */
 mongoose.connect(dbURI);
+
+/* Connection events */
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+/* Higher-order message function with close Mongoose connection */
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through' + msg);
+        callback();
+    })
+};
 
 /* SIGINT listening */
 if (process.platform === 'win32'){
@@ -17,15 +39,23 @@ if (process.platform === 'win32'){
     });
 }
 
-/* Connection events */
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + dbURI);
+/* SIGUSR2 listening for nodemon reload */
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2')
+    })
 });
 
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose default connection error: ' + err);
+/* SIGINT listening for app termination */
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
 });
 
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
+/* SIGTERM listening for Heroku app shutdown */
+process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
 });
