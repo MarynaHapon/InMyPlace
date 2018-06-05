38cba76ac0d46e919799c899b1cc82e5cc6ae18f
Maryna Hapon <marynahapon@gmail.com>
2018-06-05 21:19:23 +0300

    api / controllers --empty func

---
+++ app_api/controllers/comments.js
@@ -0,0 +1,15 @@
+module.exports.commentsCreate = function (req, res) {
+
+};
+
+module.exports.commentsReadOne = function (req, res) {
+
+};
+
+module.exports.commentsUpdateOne = function (req, res) {
+
+};
+
+module.exports.commentsDeleteOne = function (req, res) {
+
+};
---
+++ app_api/controllers/places.js
@@ -0,0 +1,19 @@
+module.exports.placesListByDistance = function (req, res) {
+
+};
+
+module.exports.placesCreate = function (req, res) {
+
+};
+
+module.exports.placesReadOne = function (req, res) {
+
+};
+
+module.exports.placesUpdateOne = function (req, res) {
+
+};
+
+module.exports.placesDeleteOne = function (req, res) {
+
+};
