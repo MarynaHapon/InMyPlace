555d265e0779b56a9835c93e9c566e322ce51440
Maryna Hapon <marynahapon@gmail.com>
2018-06-05 21:50:39 +0300

    api / models transfer

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
+require('./app_api/models/db');
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
-require('./app_server/models/db');
 
 var app = express();
 
--- app_api/controllers/places.js
+++ app_api/controllers/places.js
@@ -1,9 +1,18 @@
+var mongoose = require('mongoose');
+
+var Place = mongoose.model('Place');
+
+var sendJsonResponse = function (res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
 module.exports.placesListByDistance = function (req, res) {
 
 };
 
 module.exports.placesCreate = function (req, res) {
-
+    sendJsonResponse(res, 200, {"status": "success"})
 };
 
 module.exports.placesReadOne = function (req, res) {
