9baf1427afe6f2889ddf92c8ea4492ef588081b3
Maryna Hapon <marynahapon@gmail.com>
2018-06-05 23:00:15 +0300

    api / read item

--- app_api/controllers/places.js
+++ app_api/controllers/places.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var Place = mongoose.model('Place');
+var PlaceModel = mongoose.model('Place');
 
 var sendJsonResponse = function (res, status, content) {
     res.status(status);
@@ -8,7 +8,7 @@ var sendJsonResponse = function (res, status, content) {
 };
 
 module.exports.placesListByDistance = function (req, res) {
-
+    sendJsonResponse(res, 200, {"status": "success"})
 };
 
 module.exports.placesCreate = function (req, res) {
@@ -16,13 +16,40 @@ module.exports.placesCreate = function (req, res) {
 };
 
 module.exports.placesReadOne = function (req, res) {
+    if(req.params && req.params.placeid) {
+
+        PlaceModel
+            .findById(req.params.placeid)
+            .exec(function (err, place) {
+
+                if(!place) {
+                    sendJsonResponse(res, 404, {
+                        "message": "placeid not found"
+                    });
+                    return;
+                }
+
+                else if(err) {
+                    sendJsonResponse(res, 404, err);
+                    return;
+                }
+
+                sendJsonResponse(res, 200, place);
+            });
+    }
+
+    else {
+        sendJsonResponse(res, 404, {
+            "message": "No placeid in request"
+        });
+    }
 
 };
 
 module.exports.placesUpdateOne = function (req, res) {
-
+    sendJsonResponse(res, 200, {"status": "success"})
 };
 
 module.exports.placesDeleteOne = function (req, res) {
-
+    sendJsonResponse(res, 200, {"status": "success"})
 };
