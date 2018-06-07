a53b0a0380131579b41418f5d198047cd796a1e2
Maryna Hapon <marynahapon@gmail.com>
2018-06-07 13:15:06 +0300

    app / create item

--- app_api/controllers/places.js
+++ app_api/controllers/places.js
@@ -35,7 +35,7 @@ module.exports.placesListByDistance = function (req, res) {
 
         return;
     }
-    
+
     PlaceModel.aggregate(
         [{
             $geoNear: {
@@ -69,7 +69,35 @@ module.exports.placesListByDistance = function (req, res) {
 };
 
 module.exports.placesCreate = function (req, res) {
-    sendJsonResponse(res, 200, {"status": "success"})
+    PlaceModel.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        workHours: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2
+        }, {
+            days: req.body.days3,
+            opening: req.body.opening3,
+            closing: req.body.closing3,
+            closed: req.body.closed3
+        }]
+    }, function (err, place) {
+        if (err) {
+            sendJsonResponse(res, 400, err);
+        }
+        else {
+            sendJsonResponse(res, 201, place)
+        }
+    })
 };
 
 module.exports.placesReadOne = function (req, res) {
