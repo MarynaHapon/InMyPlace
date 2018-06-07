22f3089b8c40a28a171ca3591de4baa771bc8b63
Maryna Hapon <marynahapon@gmail.com>
2018-06-07 11:52:15 +0300

    app / get list by distance

--- app_api/controllers/places.js
+++ app_api/controllers/places.js
@@ -7,8 +7,63 @@ var sendJsonResponse = function (res, status, content) {
     res.json(content);
 };
 
+var theEarth = (function () {
+    var earthRadius = 6371;
+
+    var getDistanceFromRads = function (rads) {
+        return parseFloat(rads * earthRadius);
+    };
+
+    var getRadsFromDistance = function (distance) {
+        return parseFloat(distance / earthRadius);
+    };
+
+    return {
+        getDistanceFromRads : getDistanceFromRads,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
 module.exports.placesListByDistance = function (req, res) {
-    sendJsonResponse(res, 200, {"status": "success"})
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+
+    var geoOption = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(20),
+        num: 10
+    };
+
+    PlaceModel.aggregate(
+        [{
+            $geoNear: {
+                'near': {'type':'Point', 'coordinates':[lng, lat]},
+                'spherical': true,
+                'maxdistance': theEarth.getRadsFromDistance(20),
+                'num':10,
+                'distanceField': 'dist'
+            }
+        }], function(err, results) {
+            var locations = [];
+
+            results.forEach(function (doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRads(doc.dist),
+                    name: doc.name,
+                    address: doc.address,
+                    facilities: doc.facilities,
+                    rating: doc.rating,
+                    _id: doc._id
+                });
+            });
+
+            sendJsonResponse(res, 200, locations);
+        });
 };
 
 module.exports.placesCreate = function (req, res) {
