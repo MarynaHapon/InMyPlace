b89ebeb964c86770474e54784ceed36fb327adf7
Maryna Hapon <marynahapon@gmail.com>
2018-06-09 23:38:10 +0300

    angularjs / start

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,8 @@
 (function () {
 
+    angular.module('ngInMyPlace', []);
+
+    // google map
     function initMap() {
         var location = {lat: -25.344, lng: 131.036};
         var map = new google.maps.Map(document.querySelector('.map'), {zoom: 4, center: location});
